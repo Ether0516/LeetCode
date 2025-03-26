9624f1000fc4a233be0b958776f69edd0d1119ef
YiTong Cai <115713997+Ether0516@users.noreply.github.com>
2025-03-26 13:11:04 +0800

    Add files via upload

---
+++ 88.py
@@ -0,0 +1,22 @@
+class Solution:
+    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        i = 0
+        j = 0
+        nums3 = []
+        while i < m and j < n:
+            if nums1[i]<=nums2[j]:
+                nums3.append(nums1[i])
+                i+=1
+            else:
+                nums3.append(nums2[j])
+                j+=1
+        while i < m:
+            nums3.append(nums1[i])
+            i+=1
+        while j < n:
+            nums3.append(nums2[j])
+            j+=1
+        nums1[:] = nums3
